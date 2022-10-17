77a1dba0075935d088619390efa46465d740d2ed
dk <omnipresent9@yandex.ru>
2022-10-17 17:27:53 +0300

    5 a) Добавил дата класс с двумя свойствами b) Создал копию объекта data с) Вывел разные циклы в логкэт

--- app/src/main/java/com/dk/newprojectsearchmovie/MainActivity.kt
+++ app/src/main/java/com/dk/newprojectsearchmovie/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.dk.newprojectsearchmovie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.dk.newprojectsearchmovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn.setOnClickListener {
+        val data = Data("Danya", 33)
+        val data2 = data.copy()
 
+        binding.objectTV.text = data2.toString()
+        printCycle("Hello Kotlin")
+
+        binding.btn.setOnClickListener {
+            binding.apply {
+                dataName.text = data.name
+                dataAge.text = data.age.toString()
+            }
         }
+    }
+}
+
+fun printCycle(str: String) {
+    for (i in str.indices){
+        println(str[i])
+    }
+    for (i in 0 until  10){
+        println("$str in  $i until 10")
+    }
+    for (b in 6 downTo 0 step  3){
+        println("$str by step 3")
     }
 }
